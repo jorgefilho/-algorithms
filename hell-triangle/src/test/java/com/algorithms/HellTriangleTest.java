3a324cd02832ff96b424da2f303edd84f82a76dd
jorgefilho <papachristo@gmail.com>
2016-06-13 23:40:46 -0300

    Convert project to use maven.

---
+++ hell-triangle/src/test/java/com/algorithms/HellTriangleTest.java
@@ -0,0 +1,14 @@
+package com.algorithms;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HellTriangleTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
